e2fe7e984b7ca2223711021877af6d210b243d08
Ayushi Sinha <ayushisinha0721@gmail.com>
2025-06-13 22:57:34 +0530

    Make grid using js and flexbox

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+
+for(let i = 0; i<256; i++) {
+    let div = document.createElement('div');
+    div.classList.add('grid');
+    // div.textContent = i;
+    container.appendChild(div);
+}
+
