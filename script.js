9ba180789c147192eed1e6ca8d6c3b8f808db6a3
Ayushi Sinha <ayushisinha0721@gmail.com>
2025-06-14 00:18:03 +0530

    Add change in grid size with button press

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const container = document.querySelector('.container');
 
-for(let i = 0; i<256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('grid');
-    // div.textContent = i;
-    container.appendChild(div);
+createNewGrid = (size) => {
+    for(let i = 0; i<size*size; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid');
+        // div.textContent = i;
+        div.style.width = (100/size) + "%";
+        container.appendChild(div);
+    }
 }
 
-const grids = document.querySelectorAll('.grid');
+hoverOn = () => {
+    const grids = document.querySelectorAll('.grid');
 grids.forEach(grid => {
     grid.addEventListener("mouseover", () => {
         grid.classList.add('hovered');
         grid.style.background = "red";
     })
+})
+}
+
+createNewGrid(16);
+hoverOn();
+
+
+const button = document.querySelector('button');
+button.addEventListener("click", () => {
+    let size  = prompt("Enter the dimensions of the new grid");
+    if(size == null) {
+        return;
+    }
+    if(size > 100) {
+        window.alert("Number must be below 100");
+        return;
+    }
+    container.textContent = '';
+    createNewGrid(size);
+    hoverOn();
 })
